0a7cb3badb0a4ea5cf2f29d0ea0df5e8ac2ca61d
Abe Miessler <miessler@ecohealthalliance.org>
2017-06-12 09:51:24 -0700

    changing to snake case

--- data.py
+++ data.py
@@ -51,49 +51,49 @@ def connect_to_ftp():
   ftp.connect(url)
   ftp.login(uname, pwd)
 
-def sortByModified( aString ):
-    entryAttr = aString.split(';')
-    modified = entryAttr[2].strip()
+def sortByModified( a_string ):
+    entry_attr = a_string.split(';')
+    modified = entry_attr[2].strip()
     return modified
 
 #downloads an unprocessed file from FlightGlobal's FTP 
-def download_file(ftpEntry):
-  print "downloading", ftpEntry.name
+def download_file(ftp_entry):
+  print "downloading", ftp_entry.name
   data_directory = os.path.join(os.getcwd(), 'data')
-  fileName = ftpEntry.name.strip()
-  filepathname = os.path.join(data_directory, fileName)
+  file_name = ftp_entry.name.strip()
+  filepathname = os.path.join(data_directory, file_name)
   # try:
   #   fileOut = open(filepathname,'wb')
   # except:
   #   raise IOError("ERROR: Could not open the output file for writing")
 
   # try:
-  #   thread = Thread(target = threaded_ftp_progress, args = (ftpEntry,filepathname))
+  #   thread = Thread(target = threaded_ftp_progress, args = (ftp_entry,filepathname))
   #   thread.start()
-  #   ftp.retrbinary('RETR %s' % ftpEntry.name, fileOut.write)
+  #   ftp.retrbinary('RETR %s' % ftp_entry.name, fileOut.write)
   # except:
-  #   print "Problem downloading file", ftpEntry.name
+  #   print "Problem downloading file", ftp_entry.name
   #   raise
   # finally:
   #   thread.join()
   #   print ""
-  #   print "Done downloading", ftpEntry.name, "!!!"
+  #   print "Done downloading", ftp_entry.name, "!!!"
   #   print "**************************************"
   # fileOut.close()
 
   # backup zip to S3 if it's not already there
-  objs = list(flirt.objects.filter(Prefix=fileName))
+  objs = list(flirt.objects.filter(Prefix=file_name))
   if len(objs) == 0:
-    print "Backing up file to S3:", fileName
+    print "Backing up file to S3:", file_name
     data = open(filepathname, 'rb')
-    flirt.put_object(Key=fileName, Body=data)
+    flirt.put_object(Key=file_name, Body=data)
 
   return filepathname
 
 # extracts the CSV file from the FlightGlobal zip file
-def extract_file(filePath):
-  data_directory = os.path.dirname(filePath)
-  zip_ref = zipfile.ZipFile(filePath, 'r')
+def extract_file(file_path):
+  data_directory = os.path.dirname(file_path)
+  zip_ref = zipfile.ZipFile(file_path, 'r')
   if len(zip_ref.namelist()) != 1:
     raise IOError("ERROR: More than one file contained in Zip, should just be one CSV file")
   print "Extracting zip file: %s" % zip_ref.namelist()[0]
@@ -112,11 +112,11 @@ def read_files():
   ls = get_ftp_entries()
   CSVs = []
   for entry in ls:
-    ftpEntry = FtpEntry(entry)
-    if ftpEntry.needs_to_be_processed:
-      print "Processing file", ftpEntry.name
-      filePath = download_file(ftpEntry)
-      csv = extract_file(filePath)
+    ftp_entry = FtpEntry(entry)
+    if ftp_entry.needs_to_be_processed:
+      print "Processing file", ftp_entry.name
+      file_path = download_file(ftp_entry)
+      csv = extract_file(file_path)
       CSVs.append(csv)
   return CSVs
 
@@ -126,7 +126,7 @@ class FtpEntry:
     # db = data.FlirtDB().db
     uri = 'mongodb://%s/%s' % (host, db)
     client = pymongo.MongoClient(uri)
-    flirtDb = client['flirt']
+    flirt_db = client['flirt']
     entry =  entry.split(";")
     self.type = self.__getValue(entry[0])
     self.size = float(self.__getValue(entry[1]))
@@ -134,7 +134,7 @@ class FtpEntry:
     self.name = entry[3]
     self.extension = os.path.splitext(entry[3])[1]
     # we are assuming that zip files that do not have an entry in the "processedFiles" collection need to be processed 
-    self.needs_to_be_processed = self.extension == ".zip" and flirtDb.processedFiles.find_one({'fileName': self.name}) == None
+    self.needs_to_be_processed = self.extension == ".zip" and flirt_db.processedFiles.find_one({'fileName': self.name}) == None
 
   # extracts the value from the key/value pair. Example: 'Type=file' returns `file`
   def __getValue(self, pair):
